8ce7db0d0d4f6d22c0fd6ee1a8cffb7029932bc1
ohidursohag <ohidursohag9595@gmail.com>
2023-11-07 19:02:35 +0600

    Connected Server With MonogoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
@@ -11,6 +12,33 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 
+
+
+
+const uri = process.env.MONGO_DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      client.connect();
+      // Send a ping to confirm a successful connection
+      client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+   }
+}
+run().catch(console.dir);
 
 app.get('/', async (req, res) => {
    res.send('Welcome to Hungry Palate')
